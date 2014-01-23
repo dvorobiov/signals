2c000a5551567b8a5cc9cafb7a4646abacfa4f93
Dmitry Vorobiov <2belikespring@gmail.com>
2014-01-24 00:08:51 +0100

    add more crud operations to bookmarks

--- signals-api/src/main/scala/api/actors/BookmarkActor.scala
+++ signals-api/src/main/scala/api/actors/BookmarkActor.scala
@@ -5,7 +5,7 @@ import domain.{Bookmark, BookmarkCreate}
 import reactivemongo.api.DefaultDB
 import scala.concurrent.ExecutionContext
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
-import reactivemongo.bson.BSONObjectID
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
 import scala.util.{Success, Failure}
 import reactivemongo.api.collections.default.BSONCollection
 import spray.http.StatusCodes._
@@ -14,24 +14,55 @@ import org.joda.time.DateTime
 
 class BookmarkActor(db: => DefaultDB)(implicit executionContext: ExecutionContext) extends Actor with ActorLogging {
   lazy val collection = db.collection[BSONCollection]("bookmarks")
+  def idQuery(id: String) = BSONDocument("_id" -> id)
+
   def receive = {
     case Create(bookmark: BookmarkCreate) =>
       val bookmarkToCreate = Bookmark.fromBookmarkCreate(bookmark)
       val _sender = this.sender
       collection.insert(bookmarkToCreate).andThen {
         case Failure(e) => _sender ! Left(SignalsHttpResponse("cannot save new bookmark now", InternalServerError))
-        case Success(lastError) => _sender ! Right(SignalsHttpResponse(bookmarkToCreate, Created))
+        case Success(_) => _sender ! Right(SignalsHttpResponse(bookmarkToCreate, Created))
       }
 
     case Update(bookmark: Bookmark) =>
+      val _sender = this.sender
+      collection.update(idQuery(bookmark.id.toString), bookmark).andThen {
+        case Failure(e) => _sender ! Left(SignalsHttpResponse("cannot update", InternalServerError))
+        case Success(_) => _sender ! Right(SignalsHttpResponse("", OK))
+      }
 
     case Delete(id: String) =>
+      val _sender = this.sender
+      collection.remove(idQuery(id)).andThen {
+        case Failure(e) => _sender ! Left(SignalsHttpResponse("cannot delete", InternalServerError))
+        case Success(_) => _sender ! Right(SignalsHttpResponse("", OK))
+      }
 
     case GetAll =>
       val _sender = this.sender
-      _sender ! Right(SignalsHttpResponse(Bookmark(BSONObjectID.generate, "test", Some("hello"), List("hello"), DateTime.now), OK))
+      collection
+        .find()
+        .cursor[BSONDocument]
+        .collect[List]()
+        .andThen {
+          case Failure(e) => _sender ! Left(SignalsHttpResponse("cannot retrieve", InternalServerError))
+          case Success(bookmarks: List[Bookmark]) => _sender ! Right(SignalsHttpResponse(bookmarks, OK))
+        }
 
     case Get(id: String) =>
+      val _sender = this.sender
+      val bookmarkFutureOpt = collection
+        .find(idQuery(id))
+        .cursor[BSONDocument]
+        .headOption
+
+      val response = bookmarkFutureOpt map { bookmarkOpt =>
+        bookmarkOpt.map { bookmark =>
+          Right(SignalsHttpResponse(bookmark, OK))
+        } getOrElse(Left(SignalsHttpResponse("", NotFound)))
+      }
+      _sender ! response
 
     case _ => throw new NotImplementedException
   }
